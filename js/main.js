c31dff055db10c45b6314fae13e6cb7a767e7ed9
jchoque <jchoque@masonlive.gmu.edu>
2016-03-02 23:31:55 -0500

    1st Commit

--- js/main.js
+++ js/main.js
@@ -1,52 +1,185 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update});
+
+function preload() 
+{
+    game.load.audio('music','assets/music.mp3');
+    game.load.audio('dog_laughs','assets/dog_laughs.wav');
+    game.load.audio('cannon_hits_duck','assets/cannon_hits_duck.wav');
+    game.load.audio('cannon_fires','assets/cannon_fires.wav');
+    game.load.image('cannon', 'assets/cannon.png');
+    game.load.image('cannon_ball', 'assets/cannon_ball.png');
+    game.load.image('background', 'assets/background.png');
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
-    "use strict";
+    game.load.spritesheet('duck', 'assets/duck.png', 79, 66);
+    game.load.spritesheet('dog', 'assets/dog.png', 60, 45);
+    //game.load.image('duck', 'assets/duck.png');
+}
+
+var sprite;
+var dog;
+var dogLaughs;
+var duck;
+var music;
+var cannon_balls;
+var cannonHitsDuckSound;
+var bulletsText;
+var cannonFiresSound;
+var gameOverText;
+var killedDucksText;
+var killedDucks = 0;
+var fireRate = 800;
+var nextFire = 0;
+var numCannonBalls = 20;
+
+function create()
+{
+
+    music = game.add.audio('music');
+    cannonFiresSound = game.add.audio('cannon_fires');
+    cannonHitsDuckSound = game.add.audio('cannon_hits_duck');
+    dogLaughs = game.add.audio('dog_laughs');
+    music.play();
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    game.add.image(0,0, 'background');
+    /*cannon_balls = game.add.group();
+    cannon_balls.enableBody = true;
+    cannon_balls.physicsBodyType = Phaser.Physics.ARCADE;*/
+    //startNewRound();
+/*
+    cannon_balls.createMultiple(50, 'cannon_ball');
+    cannon_balls.setAll('checkWorldBounds', true);
+    //cannon_balls.setAll('outOfBoundsKill', true);*/
+    sprite = game.add.sprite(400, 580, 'cannon');
+    sprite.scale.setTo(0.4, 0.4);
+    sprite.anchor.set(0.5);
+
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+    sprite.body.allowRotation = false;
+
+     //ADD BULLETS TEXT
+    bulletsText = game.add.text(32, 550, 'Cannons Left: ' + numCannonBalls, { font: "25px Arial", fill: "#020202", align: "left" });
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    //ADD KILLED DUCKS TEXT
+    killedDucksText = game.add.text(600, 550, 'Killed Ducks: ' + killedDucks, { font: "25px Arial", fill: "#020202", align: "left" });
+    
+    //introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
+    //introText.anchor.setTo(0.5, 0.5);
+
+   // game.input.onDown.add(startNewRound, this);
+    //startNewRound();
+   // game.input.onDown.add(removeIntroText, this);
+    
+    
+    
+    
+    startNewRound();
+    //spawnDuck();
+}
+
+function cannonBallHitsDuck(cannon_balls, duck)
+{
+    cannon_balls.kill();
+    duck.kill();
+    cannonHitsDuckSound.play();
+    killedDucks++;
+    killedDucksText.text = 'Killed Ducks: ' + killedDucks;
+   
+    startNewRound();
+}
+
+
+
+function spawnDuck()
+{
+    
+   // duck = game.add.sprite(game.world.randomX, game.world.randomY, 'duck');
+    //duck.scale.setTo(0.3, 0.3);
+     //ADD BIRD
+    duck = game.add.sprite(game.world.randomX, game.world.randomY, 'duck');
+    //bird = game.add.sprite(400, 300, 'bird');
+    //bird.scale.setTo(0.1, 0.1);
+   // bird.anchor.setTo(0.1, 0.1);
+    var walk = duck.animations.add('walk');
+    duck.animations.play('walk', 8, true);
+    
+    
+    
+    
+    game.physics.enable(duck, Phaser.Physics.ARCADE);
+    duck.body.velocity.setTo(100,100);
+    
+    //  This makes the game world bounce-able
+    duck.body.collideWorldBounds = true;
+    
+    //  This sets the image bounce energy for the horizontal 
+    //  and vertical vectors. "1" is 100% energy return
+    duck.body.bounce.set(1);
+}
+
+function startNewRound()
+{     
+    //cannon_balls = null;
+    //this.numCannonBalls = 5;
+    //bulletsText.text = 'Bullets Left: ' + this.numCannonBalls/*cannon_balls.countDead()*/;
+   // bulletsText.text = 'Bullets Left: ' + this.numCannonBalls/*cannon_balls.countDead()*/;
+    cannon_balls = game.add.group();
+    cannon_balls.enableBody = true;
+    cannon_balls.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    cannon_balls.createMultiple(this.numCannonBalls, 'cannon_ball');
+    cannon_balls.setAll('checkWorldBounds', true);
+    //bulletsText.text = 'Bullets Left: ' +  this.numCannonBalls/*cannon_balls.countDead()*/;
+    spawnDuck();
+}
+function update() 
+{
+    
+    sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+    game.physics.arcade.overlap(cannon_balls, duck, cannonBallHitsDuck, null, this);
+    if (game.input.activePointer.isDown)
+    {
+        fire();
     }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+    /*if(this.numCannonBalls == 0)
+    {
+            cannon_balls.kill();
+            duck.kill();
+            startNewRound();
+    }*/
+}
+
+function fire() 
+{
+
+    if (game.time.now > nextFire && this.numCannonBalls > 0)
+    {
+        cannonFiresSound.play();
+        nextFire = game.time.now + fireRate;
+
+        var bullet = cannon_balls.getFirstDead();
+
+        bullet.reset(sprite.x -30, sprite.y - 50 );
+
+        game.physics.arcade.moveToPointer(bullet, 300);
+        this.numCannonBalls--;
+        bulletsText.text = 'Cannons Left: ' + this.numCannonBalls/*cannon_balls.countDead()*/;
+        //this.numCannonBalls--;
+         if(this.numCannonBalls == 0)
+        {   
+            dogLaughs.play();
+            dog = game.add.sprite(680, 330, 'dog');
+            dog.scale.setTo(2,2);
+            var walk = dog.animations.add('walk');
+            dog.animations.play('walk', 8, true);
+            duck.animations.stop();
+            gameOverText = game.add.text(game.world.centerX, 300, 'GAME OVER', { font: "40px Arial", fill: "#e5e500", align: "center" });
+            gameOverText.anchor.setTo(0.5, 0.5);
+        }
     }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
-};
+
+}
+
